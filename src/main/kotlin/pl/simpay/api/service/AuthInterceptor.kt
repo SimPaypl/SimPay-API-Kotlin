be735f72965bc0526c53030ae822c1c3ee363bf7
Rafał Więcek <gitzzz14@gmail.com>
2023-03-31 22:31:09 +0200

    Additional headers with informations

--- src/main/kotlin/pl/simpay/api/service/AuthInterceptor.kt
+++ src/main/kotlin/pl/simpay/api/service/AuthInterceptor.kt
@@ -5,6 +5,8 @@ import okhttp3.Response
 
 private const val SIM_KEY_HEADER = "X-SIM-KEY"
 private const val SIM_PASSWORD_HEADER = "X-SIM-PASSWORD"
+private const val SIM_VERSION_HEADER = "X-SIM-VERSION"
+private const val SIM_PLATFORM_HEADER = "X-SIM-PLATFORM"
 
 class AuthInterceptor(private val apiKey: String, private val simPassword: String) : Interceptor {
 
@@ -13,6 +15,8 @@ class AuthInterceptor(private val apiKey: String, private val simPassword: Strin
             .newBuilder()
             .header(SIM_KEY_HEADER, apiKey)
             .header(SIM_PASSWORD_HEADER, simPassword)
+            .header(SIM_VERSION_HEADER, "2.1.1")
+            .header(SIM_PLATFORM_HEADER, "KOTLIN")
             .build()
         return chain.proceed(request)
     }
